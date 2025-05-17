c0172fb6eb0711e1e940a5b48e0f3f63f1012dbe
Jeevan <mrsalvatore86@gmail.com>
2025-05-17 17:27:04 +0530

    updated main.py
    Added extract feature

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import tkinter as tk
+import fitz
 from tkinter import filedialog
+
+print("Welcome to Automatic Notes summarizer \n")
+print("This program will help you summarize your notes and make them more concise.\n")
+print("Please select a PDF file to extract metadata from: \n")
 
 root = tk.Tk()
 root.withdraw()  
@@ -7,13 +12,20 @@ root.withdraw()
 file_path = filedialog.askopenfilename(
     title = "Select a file:"
     )
-if file_path:
-    print(f"Selected file: {file_path}")
+
+def get_metadata(file_path):
     try:
-        with open(file_path, 'r') as file:
-            content = file.read()
-            print(f"file content is: {content}")
+        pdf = fitz.open(file_path)
+        metadata = pdf.metadata
+        for key, value in metadata.items():
+            print(f"{key}: {value}")
     except Exception as e:
-        print(f"Error reading file: {e}")
+        print(f"Error opening PDF file: {e}")
+    exit()
+
+number_input  = input("To extract metadata from a PDF file, choose 1 \n")
+if number_input == "1":
+    get_metadata(file_path)
 else:
-    print("No file selected.")
+    print("Invalid input. Please select a valid option.")
+    exit()
